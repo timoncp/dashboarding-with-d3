c687745862203d9c20a3c65c72aaab2974c827de
timoncp <timon.carneciprove@icloud.com>
2018-11-11 21:02:46 +0200

    Added more elements to programmatic model, interpreting data from props

--- src/Core/LineChart.js
+++ src/Core/LineChart.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { scaleLinear } from 'd3-scale';
-import { line as d3Line, curveMonotoneX } from 'd3-shape';
-import { range } from 'd3-array';
-import { randomUniform } from 'd3-random';
+import { line as d3Line, area as d3Area, curveMonotoneX } from 'd3-shape';
+import { max, extent } from 'd3-array';
 import { axisBottom, axisLeft } from 'd3-axis';
 import { select } from 'd3-selection';
 
@@ -15,13 +14,6 @@ const margin = { top: 25, right: 25, bottom: 25, left: 25 },
   width = svgWidth - margin.left - margin.right,
   height = svgHeight - margin.top - margin.bottom;
 
-const max = {
-  X: 20,
-  Y: 5,
-};
-
-const generateData = () => range(max.X).map(x => ({ x, y: randomUniform(max.Y)() }));
-
 const x = scaleLinear().range([0, width]),
   y = scaleLinear().range([height, 0]);
 
@@ -30,35 +22,62 @@ const line = d3Line()
   .x(d => x(d.x))
   .y(d => y(d.y));
 
-x.domain([0, max.X]);
+const area = d3Area()
+  .curve(curveMonotoneX)
+  .x(d => x(d.x))
+  .y0(y(0))
+  .y1(d => y(d.y));
 
-y.domain([0, max.Y]);
-
-const drawLine = (color) => (
+const drawLine = (data, color) => (
   <path
-    className={`line ${color}`}
-    d={line(generateData())}
+    className={'line'}
+    d={line(data)}
+    stroke={color}
   />
 );
 
-const lines = [
-  'green',
-  'orange',
-  'blue',
-];
-
-export default () => (
-  <svg width={svgWidth} height={svgHeight}>
-    <g transform={`translate(${margin.left}, ${margin.top})`}>
-      <g
-        className="axis axis--x"
-        transform={`translate(0, ${height})`}
-        ref={node => select(node).call(axisBottom(x))}
-      />
-      <g className="axis axis--y" ref={node => select(node).call(axisLeft(y))} />
-      <g className="lines">
-        { lines.map(color => drawLine(color)) }
-      </g>
-    </g>
-  </svg>
+const drawArea = (data) => (
+  <path
+    d={area(data)}
+    fill='url(#areaGradient)'
+  />
 );
+
+const DEFAULT_COLOR = 'lightseagreen';
+
+export default (props) => {
+  const data = props.data || [];
+  const color = props.color || DEFAULT_COLOR;
+
+  x.domain(extent(data, el => el.x));
+  y.domain([0, max(data, el => el.y) + 1]);
+
+  return (
+    <svg width={svgWidth} height={svgHeight}>
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+        <g
+          className="axis axis--x"
+          transform={`translate(0, ${height})`}
+          ref={node => select(node).call(axisBottom(x))}
+        />
+        <g className="axis axis--y"
+          ref={node => select(node)
+            .call(axisLeft(y).ticks(5))
+            .call(g => g.select('.domain').remove())}
+        />
+        <g className="lines">
+          { drawLine(data, color) }
+        </g>
+        <g className="areas">
+          { drawArea(data, color) }
+        </g>
+        <defs>
+          <linearGradient id='areaGradient' x1='0%' y1='15%' x2='0%' y2='100%'>
+            <stop offset='0%' stop-color={color} stop-opacity='0.6' />
+            <stop offset='100%' stop-color='white' stop-opacity='0' />
+          </linearGradient>
+        </defs>
+      </g>
+    </svg>
+  );
+};
--- src/Modules/Dashboard.js
+++ src/Modules/Dashboard.js
@@ -4,12 +4,26 @@ import LineChart from '../Core/LineChart';
 
 import './Dashboard.css';
 
+const data = [
+  { x: 0, y: 8.5 },
+  { x: 1, y: 8.5 },
+  { x: 2, y: 8.7 },
+  { x: 3, y: 8.5 },
+  { x: 4, y: 8.2 },
+  { x: 5, y: 5.0 },
+  { x: 6, y: 6.5 },
+  { x: 7, y: 5.5 },
+  { x: 8, y: 4.7 },
+  { x: 9, y: 8.5 },
+  { x: 10, y: 9.5 },
+];
+
 class Dashboard extends React.Component {
   render() {
     return (
       <div>
         <h2>Dashboard</h2>
-        <LineChart />
+        <LineChart data={data} />
       </div>
     );
   }
